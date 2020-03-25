2c2c234f3c84a38e0a219502312f717bde2d71f9
Jack_Kilby <jackiehit@163.com>
2020-03-26 07:42:57 +0800

    [example_8_2] According to Book, modify a little.

--- Chapter8/8_2/echo_eof.c
+++ Chapter8/8_2/echo_eof.c
@@ -1,10 +1,10 @@
+/* echo_eof.c -- Repeat input until end of file */
 #include <stdio.h>
 int main(void)
 {
 	int ch;
-	while ((ch = getchar()) != EOF) {
+	while ((ch = getchar()) != EOF)
 		putchar(ch);
-	}
 
 	return 0;
 }
