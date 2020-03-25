5e1058c912d2f3b3d6c9d13ea186810817d0cb5d
Jack_Kilby <jackiehit@163.com>
2020-03-25 08:45:43 +0800

    [example_5_15] Move to a new folder. [PlayCode] Test Declaration instead of Prototype

---
+++ Chapter5/5_15/PlayCode/pound.c
@@ -0,0 +1,21 @@
+/* pound.c -- Define a function with one parameter */
+#include <stdio.h>
+void pound();
+int main(void)
+{
+	int iTest = 5;
+	char cTest = '!';
+	float fTest = 5;
+
+	pound(iTest);
+	pound(cTest);
+	pound(fTest);
+
+	return 0;
+}
+void pound(int n)
+{
+	while(n-- > 0)
+		printf("#");
+	printf("\n");
+}
