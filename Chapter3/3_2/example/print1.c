758868251a9e7b96fd2491a63c5c97406085d156
Jack_Kilby <jackiehit@163.com>
2020-04-05 19:52:13 +0800

    [example_3_2] According to book, add a comment.

--- Chapter3/3_2/example/print1.c
+++ Chapter3/3_2/example/print1.c
@@ -5,11 +5,10 @@ int main(void)
 	int ten = 10;
 	int two = 2;
 
-	printf("Do it right:\n");
+	printf("Do it right:");
 	printf("%d minus %d is %d.\n", ten, two, ten-two);
-	printf("Do it wrong:\n");
-	printf("%d minus %d is %d.\n", ten);
-
+	printf("Do it wrong:");
+	printf("%d minus %d is %d.\n", ten);	//There will be warning here, lacking 2 prarms.
 
 	return 0;
 }
