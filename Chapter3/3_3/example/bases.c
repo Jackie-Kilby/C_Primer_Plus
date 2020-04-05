fff0027ab941a663f198923da3740ebebe87021e
Jack_Kilby <jackiehit@163.com>
2020-04-05 20:03:19 +0800

    [example_3_3] Modify: There is no %#d in C like %#x.

--- Chapter3/3_3/example/bases.c
+++ Chapter3/3_3/example/bases.c
@@ -1,10 +1,11 @@
+/* print DEC 100 as HEX and OCT */
 #include <stdio.h>
 int main(void)
 {
 	int x = 100;
 
 	printf("dec = %d, hex = %x, oct= %o.\n", x, x, x);
-	printf("dec = %#d, hex = %#x, oct = %#o.\n", x, x, x);
+	printf("dec = %d, hex = %#x, oct = %#o.\n", x, x, x);
 
 	return 0;
 }
