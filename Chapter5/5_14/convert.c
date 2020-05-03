0d4d76502a77bfc8d2263d31d41c3b31580c80e9
Jack_Kilby <jackiehit@163.com>
2020-05-03 23:29:33 +0800

    [example_5_14] According to BOOK, change the %f to %2.2f to make the result readable. This auto-convert example is finished now.

--- Chapter5/5_14/convert.c
+++ Chapter5/5_14/convert.c
@@ -7,11 +7,11 @@ int main(void)
 	float fl;
 
 	ch = i = fl = 'C';
-	printf("ch = %c, i = %d, fl = %f\n", ch, i, fl);
+	printf("ch = %c, i = %d, fl = %2.2f\n", ch, i, fl);
 	ch = ch + 1;
-	i = ch*2 + fl;
-	fl = ch*2 + i;
-	printf("ch = %c, i = %d, fl = %f\n", ch, i, fl);
+	i = ch * 2 + fl;
+	fl = ch * 2.0 + i;
+	printf("ch = %c, i = %d, fl = %2.2f\n", ch, i, fl);
 	ch = 1107;
 	printf("Now ch = %c\n", ch);
 	ch = 80.89;
