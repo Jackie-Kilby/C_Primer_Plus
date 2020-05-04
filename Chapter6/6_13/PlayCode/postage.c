66a93fe75958dd3a7842fa80a01e97b19b865f84
Jack_Kilby <jackiehit@163.com>
2020-05-04 22:28:02 +0800

    [PlayCode_6_13] Change the word round_up to term ceiling.

--- Chapter6/6_13/PlayCode/postage.c
+++ Chapter6/6_13/PlayCode/postage.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define ROUND_UP_METHOD	1
+#define CEILING_METHOD	1
 
-static float round_up(float fl);
+static float ceiling(float fl);
 
 int main(void)
 {
@@ -14,22 +14,22 @@ int main(void)
 	while (1 != scanf("%f", &ounces)) {
 		printf("Sorry, please check your input is an integar, and input again:");
 	}
-	ounces = round_up(ounces);
+	ounces = ceiling(ounces);
 	cost = (FIRST_OZ + (ounces - 1) * NEXT_OZ) / 100.0;
 	printf("The cost is %.2f\n", cost);
 
 	return 0;
 }
-#if (ROUND_UP_METHOD == 1)
-static float round_up(float fl)
+#if (CEILING_METHOD == 1)
+static float ceiling(float fl)
 {
 	if ((int)fl != fl)
 		return (int)fl + 1.0;
 	else 
 		return fl;
 }
-#elif (ROUND_UP_METHOD == 2)
-static float round_up(float fl)
+#elif (CEILING_METHOD == 2)
+static float ceiling(float fl)
 {
 	if (0 != ((int)(fl*100)%100))
 		return (int)fl +1.0;
