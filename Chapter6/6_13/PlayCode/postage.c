ec5984da37fd3bfbff58f9bdd7b4dac034c3fbca
Jack_Kilby <jackiehit@163.com>
2020-05-04 22:02:45 +0800

    [PlayCode_6_13] Use round up(向上取整) method to solve the problem of inputting float number.

--- Chapter6/6_13/PlayCode/postage.c
+++ Chapter6/6_13/PlayCode/postage.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
+static float round_up(float fl);
+
 int main(void)
 {
 	int const FIRST_OZ = 46;
 	int const NEXT_OZ = 20;
-	int ounces, cost;
+	float ounces, cost;
 
 	printf("What's the weight of your package?\n");
-	while (1 != scanf("%d", &ounces)) {
+	while (1 != scanf("%f", &ounces)) {
 		printf("Sorry, please check your input is an integar, and input again:");
 	}
-	cost = FIRST_OZ + (ounces - 1) * NEXT_OZ;
-	printf("The cost is %.2f\n", cost/100.0);
+	ounces = round_up(ounces);
+	cost = (FIRST_OZ + (ounces - 1) * NEXT_OZ) / 100.0;
+	printf("The cost is %.2f\n", cost);
 
 	return 0;
 }
+
+static float round_up(float fl)
+{
+	if ((int)fl != fl) {
+		return (int)fl + 1.0;
+	}
+	else 
+		return (int)fl;
+}
+
