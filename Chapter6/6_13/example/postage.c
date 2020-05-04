cfd4dcff1fa20a88f4dfaa8ee4e56d765888344d
Jack_Kilby <jackiehit@163.com>
2020-05-04 21:18:58 +0800

    [example_6_13] According to BOOK, use Refactor method-RenameVariables to rename the const variables.

--- Chapter6/6_13/example/postage.c
+++ Chapter6/6_13/example/postage.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-	int const FIRST_ONCE = 46;
-	int const ADD_ONCE = 26;
+	int const FIRST_OZ = 46;
+	int const NEXT_OZ = 20;
+	int ounces, cost;
 
-	int onces, cost;
-	for(onces = 1, cost = FIRST_ONCE; onces < 16; cost += ADD_ONCE, onces++)
-		printf("once = %d, cost %4.2f\n", onces, cost/100.0);
+	printf("Onces cost\n");
+	for(ounces = 1, cost = FIRST_OZ; ounces <= 16; cost += NEXT_OZ, ounces++)
+		printf("%-5d $%4.2f\n", ounces, cost/100.0);
 
 	return 0;
 }
