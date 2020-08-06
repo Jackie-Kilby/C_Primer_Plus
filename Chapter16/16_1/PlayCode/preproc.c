bda5fe0dddadb020e9924ce178e4ef4764c7ab2c
Jack Kilby <jackiehit@163.com>
2020-08-06 09:50:07 +0800

    [PlayCode_16_1] Use const to replace #define

---
+++ Chapter16/16_1/PlayCode/preproc.c
@@ -0,0 +1,21 @@
+/* A simple example of pre-process using #define */
+#include <stdio.h>
+
+#define TWO 2 			//Using comment is ok
+#define PX printf("X is %d\n", x);
+#define OW "Consistency is the last refuge of \
+the unimaginative. - Oscar Wilde"
+
+#define FOUR TWO*TWO
+#define FMT "X is %d\n"
+const char * fmt = "X is %d\n";
+int main(void)
+{
+	int x = TWO;
+	PX;
+
+	x = FOUR;
+	printf("%s\n", OW);
+	printf(fmt, x);
+	printf("TWO:OW\n");
+}
