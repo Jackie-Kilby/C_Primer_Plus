4bd1d5d8199c71440eab9792a737aca2ca6a43c3
Jack Kilby <jackiehit@163.com>
2020-08-06 09:40:04 +0800

    [exaple_16_1] According to BOOK, make some modification.

--- Chapter16/16_1/example/preproc.c
+++ Chapter16/16_1/example/preproc.c
@@ -1,18 +1,20 @@
 /* A simple example of pre-process using #define */
 #include <stdio.h>
 
-#define TWO 2
+#define TWO 2 			//Using comment is ok
 #define PX printf("X is %d\n", x);
-#define WX "Consistency is \
-a new line\n"
+#define OW "Consistency is the last refuge of \
+the unimaginative. - Oscar Wilde"
+
 #define FOUR TWO*TWO
-#define STR "X is %d\n"
+#define FMT "X is %d\n"
 int main(void)
 {
 	int x = TWO;
 	PX;
 
-	printf(WX);
-	printf(STR, FOUR);
-	printf("Four is %d\n", FOUR);
+	x = FOUR;
+	printf("%s\n", OW);
+	printf(FMT, x);
+	printf("TWO:OW\n");
 }
